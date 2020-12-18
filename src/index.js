69399d0cca9bce0a0531e8a918cc6ef33e590262
Vladimir Riz <resin.fhp@gmail.com>
2020-12-18 19:00:27 +0200

    init: start covid dashboard project

---
+++ src/index.js
@@ -0,0 +1,3 @@
+const body = document.querySelector('body');
+
+body.innerHTML = 'Hello!';
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  // mode: 'development',
+  entry: './src/index.js',
+  devtool: 'eval-source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
