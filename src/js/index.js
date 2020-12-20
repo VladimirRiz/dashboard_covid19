a63bb23f8e5684443a9f13dba23f64bc639bf864
Vladimir Riz <resin.fhp@gmail.com>
2020-12-20 11:31:10 +0200

    feat: add container and menu

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+const navList = document.querySelector('.navigation_list');
+const navButton = document.querySelector('.navigation_button');
+
+navButton.addEventListener('click', () => {
+    navButton.classList.toggle('change');
+    navList.classList.toggle('active');
+})
