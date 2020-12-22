d116447e1d6d4149f2766eae0b5a9c34a922d4f9
Vladimir Riz <resin.fhp@gmail.com>
2020-12-22 20:18:53 +0200

    feat: add total info cases for all countries

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -24,13 +24,6 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => /* binding */ cases
 /* harmony export */ });
-// async function getUser(name) {
-//   const country = name.toLowerCase().replace(/ /g, '-');
-//   const response = await fetch(`https://api.covid19api.com/total/country/${country}/status/confirmed`);
-//   const data = await response.json();
-//   return data;
-// }
-
 function cases() {
   const casesInfo = document.querySelector('.cases__info');
   const getCountry = fetch('https://api.covid19api.com/summary');
@@ -38,39 +31,11 @@ function cases() {
     .then(({ Countries }) => {
       const arr = [...Countries];
       arr.sort((a, b) => ((a.TotalConfirmed < b.TotalConfirmed) ? 1 : -1));
-      console.log(arr);
       arr.forEach((x) => {
         casesInfo.insertAdjacentHTML('beforeend', `<div class="country"><p class="number">${x.TotalConfirmed}</p><p class="name">${x.Country}</p></div>`);
       });
     });
 }
-
-// Call the API
-// fetch('https://jsonplaceholder.typicode.com/posts/5').then(function (response) {
-// 	if (response.ok) {
-// 		return response.json();
-// 	} else {
-// 		return Promise.reject(response);
-// 	}
-// }).then(function (data) {
-
-// 	// Store the post data to a variable
-// 	post = data;
-
-// 	// Fetch another API
-// 	return fetch('https://jsonplaceholder.typicode.com/users/' + data.userId);
-
-// }).then(function (response) {
-// 	if (response.ok) {
-// 		return response.json();
-// 	} else {
-// 		return Promise.reject(response);
-// 	}
-// }).then(function (userData) {
-// 	console.log(post, userData);
-// }).catch(function (error) {
-// 	console.warn(error);
-// });
 
 
 /***/ }),
--- src/js/cases.js
+++ src/js/cases.js
@@ -1,10 +1,3 @@
-// async function getUser(name) {
-//   const country = name.toLowerCase().replace(/ /g, '-');
-//   const response = await fetch(`https://api.covid19api.com/total/country/${country}/status/confirmed`);
-//   const data = await response.json();
-//   return data;
-// }
-
 export default function cases() {
   const casesInfo = document.querySelector('.cases__info');
   const getCountry = fetch('https://api.covid19api.com/summary');
@@ -12,36 +5,8 @@ export default function cases() {
     .then(({ Countries }) => {
       const arr = [...Countries];
       arr.sort((a, b) => ((a.TotalConfirmed < b.TotalConfirmed) ? 1 : -1));
-      console.log(arr);
       arr.forEach((x) => {
         casesInfo.insertAdjacentHTML('beforeend', `<div class="country"><p class="number">${x.TotalConfirmed}</p><p class="name">${x.Country}</p></div>`);
       });
     });
 }
-
-// Call the API
-// fetch('https://jsonplaceholder.typicode.com/posts/5').then(function (response) {
-// 	if (response.ok) {
-// 		return response.json();
-// 	} else {
-// 		return Promise.reject(response);
-// 	}
-// }).then(function (data) {
-
-// 	// Store the post data to a variable
-// 	post = data;
-
-// 	// Fetch another API
-// 	return fetch('https://jsonplaceholder.typicode.com/users/' + data.userId);
-
-// }).then(function (response) {
-// 	if (response.ok) {
-// 		return response.json();
-// 	} else {
-// 		return Promise.reject(response);
-// 	}
-// }).then(function (userData) {
-// 	console.log(post, userData);
-// }).catch(function (error) {
-// 	console.warn(error);
-// });
