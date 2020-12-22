933d622131dda186179d87c3d4d11c10f4be22a0
mrinex <mrinex@mail.ru>
2020-12-23 01:02:05 +0300

    feat: add left side of the first block\

--- src/js/API/covid.summary.js
+++ src/js/API/covid.summary.js
@@ -1,20 +1,22 @@
 import Covid from './Covid';
 
-async function getCovidSummary() {
+async function covidSummary() {
   const url = 'https://api.covid19api.com/summary';
   try {
     const response = await fetch(url);
     const content = await response.json();
-    return new Covid({
+    const summary = new Covid({
       error: null,
       data: content,
     });
+    summary.set();
   } catch (error) {
-    return new Covid({
+    const summary = new Covid({
       error,
       data: null,
     });
+    summary.error();
   }
 }
 
-export default getCovidSummary;
+export default covidSummary;
