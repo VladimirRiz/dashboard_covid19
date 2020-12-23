82335ad4ff4328fc690985a8cf2e564e1394801b
Vladimir Riz <resin.fhp@gmail.com>
2020-12-23 18:15:56 +0200

    gh-pages

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -32,23 +32,24 @@ class Covid {
 
   set() {
     const death = document.querySelector('.death .info');
+    const globalCases = document.querySelector('.global__cases');
+    globalCases.innerHTML = `<div>Global Cases</div><h2 class="text-danger">${this.data.Global.TotalConfirmed}</h2>`;
 
     const container = document.createElement('div');
     container.innerHTML = `
       <div class="global-deaths">
-        <h4>Global Deaths</h4>
-        <h1>${this.data.Global.TotalDeaths}</h1>
+        <div>Global Deaths</div>
+        <h2>${this.data.Global.TotalDeaths}</h2>
       </div>
       <div class="countries-deaths">
         ${this.data.Countries.map((node) => `
             <div class="country">
-              <h3>${node.TotalDeaths} deaths</h3>
-              <h4>${node.Country}</h4>
+            <p class="name">${node.TotalDeaths} deaths</p>
+            <p class="name">${node.Country}</p>
             </div>
           `).join(' ')}
       </div>
     `;
-
     death.appendChild(container);
   }
 
